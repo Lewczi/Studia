96e8ded9d3628f6617cfcaa5e5e65a06b5cab893
Lewczi <p.lech8@gmail.com>
2025-11-30 13:59:07 +0100

    Zadanie_1

---
+++ Python_typing/Zadanie_1.py
@@ -0,0 +1,6 @@
+def przywiatanie(name: str, surname: str):
+    return f"Cześć {name} {surname}"
+
+# Uruchomienie i wyświetlenie
+greeting_result = przywiatanie("Jan", "Kowalski")
+print(greeting_result)
