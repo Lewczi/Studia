a716d80e6a5c14fce437a81d9e2819ea4d22166f
Lewczi <p.lech8@gmail.com>
2025-12-07 08:50:07 +0100

    PEP8

--- Zadanie_D.py
+++ Zadanie_D.py
@@ -1,10 +1,11 @@
 def wypisz_co_drugi(liczby):
-   
+
     co_drugi = liczby[::2]
-    
+
     print("Co drugi element:")
     for element in co_drugi:
         print(element)
+
 
 dane = list(range(10, 20))
 print(f"Dane wejściowe: {dane}")
