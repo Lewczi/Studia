4d068d0921701a3a54f615f16f257628fdeaec3c
Lightman144 <bartlomiej.babczyk@edu.uekat.pl>
2025-12-20 10:05:16 +0100

    pierwsza_funckja

---
+++ funkcje_do_testów/funkcje.py
@@ -0,0 +1,8 @@
+def is_palindrome(text: str) -> bool:
+    wyczysczone = "".join.(text.lower().split())
+    wyczszczone = wyczszczone[::-1]
+    return wyczyszczone 
+
+
+def fibonacci(n: int) -> int: 
+    
